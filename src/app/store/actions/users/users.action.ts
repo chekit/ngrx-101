b326edf86b39860433e053033682760e826a8bd6
Anton Cherepov <chekitdnb@gmail.com>
2018-01-18 22:43:16 +0300

    extend actions, fix packages versions

--- src/app/store/actions/users/users.action.ts
+++ src/app/store/actions/users/users.action.ts
@@ -1,19 +1,18 @@
 import { Action } from '@ngrx/store';
+import { UserModel } from '../../../models/users/user.model';
 
 export enum UsersListActions {
 	LOAD_USERS = '[Users] Load users list',
+	LOAD_USERS_SUCCESS = '[Users] Load users list succeded',
 	LOAD_USERS_ERROR = '[Users] Load users list failed',
-	LOAD_USERS_SUCCESS = '[Users] Load users list succeded'
+
+	LOAD_USER = '[User] Load user info',
+	LOAD_USER_SUCCESS = '[User] Load user info succeded',
+	LOAD_USER_ERROR = '[User] Load user info failed',
 };
 
 export class LoadUsers implements Action {
 	readonly type: string = UsersListActions.LOAD_USERS;
-}
-
-export class LoadUsersError implements Action {
-	readonly type: string = UsersListActions.LOAD_USERS_ERROR;
-
-	constructor(public payload: any) {}
 }
 
 export class LoadUsersSuccess implements Action {
@@ -22,4 +21,26 @@ export class LoadUsersSuccess implements Action {
 	constructor(public payload: any) {}
 }
 
-export type UsersActions = LoadUsers | LoadUsersError | LoadUsersSuccess;
+export class LoadUsersError implements Action {
+	readonly type: string = UsersListActions.LOAD_USERS_ERROR;
+
+	constructor(public payload: any) {}
+}
+
+export class LoadUser implements Action {
+	readonly type: string = UsersListActions.LOAD_USER;
+}
+
+export class LoadUserSuccess implements Action {
+	readonly type: string = UsersListActions.LOAD_USER_SUCCESS;
+
+	constructor(public payload: UserModel) {}
+}
+
+export class LoadUserError implements Action {
+	readonly type: string = UsersListActions.LOAD_USER_ERROR;
+
+	constructor(public payload: any) {}
+}
+
+export type UsersActions = LoadUsers | LoadUsersError | LoadUsersSuccess | LoadUser | LoadUserError | LoadUserSuccess;
